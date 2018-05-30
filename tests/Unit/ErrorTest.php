1899b71624e7e1a1e247bdc7cb92cee57a2ac166
Michael Drennen <michaeldrennen74@gmail.com>
2018-05-29 21:59:11 -0600

    Added tests.

--- tests/Unit/ErrorTest.php
+++ tests/Unit/ErrorTest.php
@@ -35,4 +35,31 @@ class ErrorTest extends TestCase {
         $this->assertEquals( 667, $array[ 'errors' ][ 0 ][ 'code' ] );
     }
 
+
+    /**
+     * @test
+     */
+    public function errorShouldHaveAllTheOptions() {
+        $user  = new User( 74, "Mike", TRUE );
+        $array = \MichaelDrennen\JSONAPI\Response::create()
+                                                 ->setData( $user )
+                                                 ->transformWith( new UserTransformer() )
+                                                 ->addError( \MichaelDrennen\JSONAPI\Error::create()
+                                                                                          ->setCode( 667 )
+                                                                                          ->addLink( 'https://google.com',
+                                                                                                     'testLink1' )
+                                                                                          ->addLink( 'https://yahoo.com' )
+                                                                                          ->setAbout( "This is the about text for this error." )
+                                                                                          ->setDetail( "This is the detail regarding this error." )
+                                                                                          ->setId( 77 )
+                                                                                          ->setSource( NULL )
+                                                                                          ->setStatus( 124523 )
+                                                                                          ->setTitle( "This is the title of the error." )
+                                                 )
+                                                 ->toArray();
+
+        $this->assertEquals( 667, $array[ 'errors' ][ 0 ][ 'code' ] );
+        $this->assertEquals( 77, $array[ 'errors' ][ 0 ][ 'id' ] );
+    }
+
 }
